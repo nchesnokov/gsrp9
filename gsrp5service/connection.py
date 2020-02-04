c866f04217c6f4cae1eed5af4d9817af5aa264c0
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2020-02-04 16:28:25 +0300

    Modify shema of models

--- gsrp5service/connection.py
+++ gsrp5service/connection.py
@@ -35,24 +35,24 @@ class Cursor(object):
 		if not self.conn or self.conn and self.conn.closed:
 			if self.sslmode:
 				if self.user == 'root':
-					if dsn:
+					if self.dsn:
 						self.conn = psycopg2.connect(dsn = self.dsn)
 					else:
 						self.conn = psycopg2.connect(dsn = self.dsn, database = self.database, user = self.user, host = self.host, port = self.port, connection_factory = psycopg2.extensions.connection,sslmode=self.sslmode,sslrootcert=self.sslrootcert,sslcert=self.sslcert,sslkey=self.sslkey)
 				else:
 					#self.conn = psycopg2.connect(dsn = self.dsn, database = self.database, user = self.user, password = self.password, host = self.host, port = self.port, connection_factory = psycopg2.extensions.connection,sslmode=self.sslmode,sslrootcert=self.sslrootsert,sslcert=self.sslcert,sslkey=self.sslkey)
-					if dsn:
+					if self.dsn:
 						self.conn = psycopg2.connect(dsn = self.dsn)
 					else:
 						self.conn = psycopg2.connect(dsn = self.dsn, database = self.database, user = self.user, password = self.password, host = self.host, port = self.port, connection_factory = psycopg2.extensions.connection,sslmode=self.sslmode,sslrootcert=self.sslrootcert,sslcert=self.sslcert,sslkey=self.sslkey)
 			else:
 				if self.user == 'root':
-					if dsn:
+					if self.dsn:
 						self.conn = psycopg2.connect(dsn = self.dsn)
 					else:
 						self.conn = psycopg2.connect(dsn = self.dsn, database = self.database, user = self.user, host = self.host, port = self.port, connection_factory = psycopg2.extensions.connection)
 				else:
-					if dsn:
+					if self.dsn:
 						self.conn = psycopg2.connect(dsn = self.dsn)
 					else:
 						self.conn = psycopg2.connect(dsn = self.dsn, database = self.database, user = self.user, password = self.password, host = self.host, port = self.port, connection_factory = psycopg2.extensions.connection)
