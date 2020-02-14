d16c476cf7f183067271350a22d8409b4b75edcf
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2020-02-14 16:01:06 +0300

    Modify shema of models

--- gsrp5service/services/models/cache.py
+++ gsrp5service/services/models/cache.py
@@ -825,9 +825,8 @@ class MCache(object):
 
 	def _m2m_add(self,container,fields,obj,rel,id1,id2):
 		
-		print('M2M-ADD:',container,fields,obj,rel,id1,id2)
-		
 		row = self._pool.get(obj).read(self._cr,self._pool,self._uid,id2,fields,self._context)
+		print('M2M-ADD:',container,fields,obj,rel,id1,id2,row)
 		
 		p = container.split('.')
 		self._data._cdata[self._data._cnames[container]].append(row)
