4cc3d6944c540a4e87d71cd83c0976eba4521706
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2020-02-27 11:45:46 +0300

    Message length from websocket

--- gsrp5service/services/models/cache.py
+++ gsrp5service/services/models/cache.py
@@ -1372,12 +1372,13 @@ class MCache(object):
 		oid = self._data._cdata[parent]['id']
 		rel = self._pool.get(self._data._cmodels[parent])._columns[cn].rel
 		recname = self._pool.get(self._data._cmodels[parent])._getRecNameName()
-		data[rel] = {'id':oid,'name':recname}
+		data[rel] = oid
 		m = self._pool.get(model)
 		excl_fields = m._o2mfields + m._m2mfields
 		for k in filter(lambda x: x not in excl_fields,item['__data__'].keys()):
 			if m._columns[k]._type in ('many2one','related'):
-				data[k] = item['__data__'][k]['id']
+				if k != rel:
+					data[k] = item['__data__'][k]['id']
 		
 		print('DATA:',data)
 		r = _createRecord(m,self._cr,self._pool,self._uid,data,self._context)
