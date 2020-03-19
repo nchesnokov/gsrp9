259f42858904ecc52df8a00d1e289bbda6fc3fdd
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2020-03-19 18:14:41 +0300

    Modify rules

--- gsrp5service/services/models/cache.py
+++ gsrp5service/services/models/cache.py
@@ -885,7 +885,7 @@ class DCacheDict(object):
 						elif type(aa) == dict:
 							for s in aa.keys():
 								sn = m._getStateName()
-								if s == self._data._cdata[path][sn]:
+								if s == self._cdata[path][sn]:
 									if type(aa[s]) == dict:
 										for s1 in aa[s].keys():
 											if aa[s][s1]:
