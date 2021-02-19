5a63c9016004a1ec115cdb95a9854a6201cad1f5
Nikolay Chesnokov <nikolaychesnokov@gmail.com>
2021-02-19 17:22:23 +0300

    Model call modify

--- gsrp5service/components/gens/tr.py
+++ gsrp5service/components/gens/tr.py
@@ -3,7 +3,9 @@ import logging
 from os.path import join as opj
 
 import polib
-from yandex import Translater
+#from yandex import Translater
+
+from deep_translator import GoogleTranslator
 
 import datetime
 from datetime import date,time,datetime
@@ -12,9 +14,13 @@ import web_pdb
 
 _logger = logging.getLogger('listener.' + __name__)
 
-tr = Translater.Translater()
-tr.set_key('trnsl.1.1.20181211T195911Z.dd221d8c2a232623.1e176c0b53c78e901d04472cdf7483c7c0f9a0b8') # Api key found on https://translate.yandex.com/developers/keys
-tr.set_from_lang('en')
+#tr = Translater.Translater()
+#tr.set_key('trnsl.1.1.20181211T195911Z.dd221d8c2a232623.1e176c0b53c78e901d04472cdf7483c7c0f9a0b8') # Api key found on https://translate.yandex.com/developers/keys
+#tr.set_from_lang('en')
+
+langs_list = dict(list(map(lambda x: list(reversed(x)),GoogleTranslator().get_supported_languages(as_dict=True).items())))
+
+
 
 def _get_mt(p):
 	res = {}
@@ -30,7 +36,8 @@ def _download_i18n_tr(path,module):
 	if os.path.exists(opj(path,module,'i18n','po.pot')):
 		po = polib.pofile(opj(path,module,'i18n','po.pot'))
 	
-		for lang in list(map(lambda x: x.split('-')[1],filter(lambda x: x[:3] == 'en-',tr.get_langs()))):
+		#for lang in list(map(lambda x: x.split('-')[1],filter(lambda x: x[:3] == 'en-',tr.get_langs()))):
+		for lang in langs_list.keys():
 			mt = _get_mt(opj(path,module,'i18n','%s.po') % (lang,))
 			#web_pdb.set_trace()
 			#print('MT:',mt)
@@ -48,22 +55,37 @@ def _download_i18n_tr(path,module):
 			    'Content-Transfer-Encoding': '8bit',
 			}
 	
-			tr.set_to_lang(lang)
-			
+			#tr.set_to_lang(lang)
+			tr_msgid = []
+			tr_batch = []
+			tr_list = []
+			tr_occurrences = []
 			for entry in po:
-				#print('entry.occurrences:',entry.occurrences)
+				tr_msgid.append(entry.msgid)
+				tr_occurrences.append(entry.occurrences)
 				if entry.msgid in mt:
-					msgstr = mt[entry.msgid]
-				else:
-					tr.set_text(entry.msgid)
-					msgstr=tr.translate()
-					mt[entry.msgid] = msgstr
-	
+					tr_list.append(mt[entry.msgid])
+
+			for entry in po:
+				tr_msgid.append(entry.msgid)
+				tr_occurrences.append(entry.occurrences)
+				if not entry.msgid in mt:
+					tr_batch.append(entry.msgid)
+
+
+			tr_list.extend(YandexTranslator(yandex_key).translate_batch(source='en',target=lang,batch=tr_batch))
+			for msgid,msgstr,occurrences in zip(tr_msgid,tr_list,tr_occurrences):
 				out_entry = polib.POEntry(
-				    msgid=entry.msgid,
+				    msgid=msgid,
 				    msgstr=msgstr,
-				    occurrences=entry.occurrences
+				    occurrences=occurrences
 				)
+
+				# out_entry = polib.POEntry(
+				    # msgid=entry.msgid,
+				    # msgstr=msgstr,
+				    # occurrences=entry.occurrences
+				# )
 				out.append(out_entry)
 	
 	
